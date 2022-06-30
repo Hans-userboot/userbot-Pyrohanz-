2003cedea3d660530424df5f0ab5f4691b53c040
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2022-06-30 17:41:30 +0700

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,96 @@
+# Credits: @HanzUserbot
+
+# Copyright (C) 2022 userbot-Pyrohanz
+
+#
+
+# This file is a part of < https://github.com/Hans-userboot/ >
+
+# PLease read the GNU Affero General Public License in
+
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+
+#
+
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from base64 import b64decode
+
+from distutils.util import strtobool
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "⚡️")
+
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/9dc4e335feaaf6a214818.jpg")
+
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
+
+API_HASH = getenv("API_HASH")
+
+API_ID = int(getenv("API_ID", ""))
+
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+
+if not BLACKLIST_CHAT:
+
+    BLACKLIST_CHAT = [-1001473548283]
+
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+
+BOT_VER = "0.1.0@main"
+
+BRANCH = "main"
+
+CHANNEL = getenv("CHANNEL", "Lunatic0de")
+
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+
+DB_URL = getenv("DATABASE_URL", "")
+
+GIT_TOKEN = getenv(
+
+    "GIT_TOKEN",
+
+    b64decode("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==").decode(
+
+        "utf-8"
+
+    ),
+
+)
+
+GROUP = getenv("GROUP", "SharingUserbot")
+
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+
+REPO_URL = getenv(
+
+    "REPO_URL",
+
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL3Byb2plY3RtNG4vUHlyb01hbg==").decode("utf-8"),
+
+)
+
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+
